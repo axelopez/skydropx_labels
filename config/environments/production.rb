e0469f5f065a39beb5f6853aa3a1c56f47318a80
axelopez <axelopez@gmail.com>
2021-11-26 01:11:40 -0600

    server produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   Redis.current = Redis.new(url: ENV.fetch("REDISCLOUD_URL"){ "redis://redis:6379" })
-  APP_URL = "https://skydropx-labels.herokuapp.com/"
+  APP_URL = "https://skydropx-labels.herokuapp.com"
 end
